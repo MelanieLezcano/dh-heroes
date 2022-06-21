5d0f95575efa549d69461fbfb70d5077ffeee869
melanieLezcano <melanie.s.lezcano@gmail.com>
2022-06-20 21:38:36 -0300

    rutas añadidas

--- app.js
+++ app.js
@@ -1,7 +1,51 @@
+/* / -> Debe direccionar al recurso index.html. 
+●/babbage -> Debe direccionar al recurso babbage.html 
+●/berners-lee -> Debe direccionar al recurso berners-lee.html. 
+●/clarke -> Debe direccionar al recurso clarke.html. 
+●/hamilton -> Debe direccionar al recurso hamilton.html. 
+●/hopper -> Debe direccionar al recurso hopper.html. 
+●/lovelace -> Debe direccionar al recurso lovelace.html. 
+●/turing-> Debe direccionar al recurso turing.html. */
+
+
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.get('/',(req,res) => res.send('dadada'))
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/babbage',(req,res) => {
+    res.sendFile(path.join(__dirname, 'babbage.html'));
+});
+
+app.get('/berners-lee',(req,res) => {
+    res.sendFile(path.join(__dirname, 'berners-lee.html'));
+});
+
+app.get('/clarke',(req,res) => {
+    res.sendFile(path.join(__dirname, 'clarke.html'));
+});
+
+app.get('/hamilton',(req,res) => {
+    res.sendFile(path.join(__dirname, 'hamilton.html'));
+});
+
+app.get('/hopper',(req,res) => {
+    res.sendFile(path.join(__dirname, 'hopper.html'));
+});
+
+app.get('/lovelace',(req,res) => {
+    res.sendFile(path.join(__dirname, 'lovelace.html'));
+});
+
+app.get('/turing',(req,res) => {
+    res.sendFile(path.join(__dirname, 'turing.html'));
+});
+
+
+
 
 app.listen(3030,()=> console.log("servidor levantado")
 );
